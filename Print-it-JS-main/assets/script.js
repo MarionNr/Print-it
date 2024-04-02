b20252c22b27d63c969939ad25cd884ddc10d5c5
MarionNr <158274621+MarionNr@users.noreply.github.com>
2024-04-02 16:34:02 +0200

    Add files via upload

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -16,3 +16,61 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const bannerImg = document.querySelector('.banner-img');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot'); // Sélectionnez tous les points
+
+let currentIndex = 0;
+
+// Fonction pour mettre à jour les dots
+function updateDots(index) {
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add('dot_selected'); // Ajoutez style selected pour le dot selectionné
+        } else {
+            dot.classList.remove('dot_selected'); // Supprimez style selected pour les autres dots
+        }
+    });
+}
+
+// Fonction pour mettre à jour les dots, l'image et le texte
+function updateCarousel(index, direction) {
+      //défilement à l'infini
+      if (currentIndex === -1 && direction === 'left') {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex === slides.length && direction === 'right') {
+        currentIndex = 0;
+    }
+
+    // changer l'image
+    const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.src = imagePath;
+    bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+    // changer le texte
+    const tagLine = slides[currentIndex].tagLine;
+    document.querySelector('p').innerHTML = tagLine;
+
+    console.log(`Clic sur la flèche ${direction}`);
+}
+
+// Gestionnaire d'événement pour le clic sur la flèche gauche
+arrowLeft.addEventListener('click', function () {
+    currentIndex = (currentIndex - 1);
+    updateCarousel(currentIndex, 'left');
+    updateDots(currentIndex); // Mettez à jour les dots
+});
+
+// Gestionnaire d'événement pour le clic sur la flèche droite
+arrowRight.addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) ;
+    updateCarousel(currentIndex, 'right');
+    updateDots(currentIndex); // Mettez à jour les dots
+});
+
+
+// Afficher la première diapositive au chargement de la page
+updateCarousel(currentIndex, 'démarrage');
+updateDots(currentIndex); // Mettez à jour les dots pour la première diapositive
